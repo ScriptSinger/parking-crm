75c5d8a5b08b1a55c875ffa5bd03882159f40424
ScriptSinger <ufamasters102@gmail.com>
2025-06-19 23:53:26 +0500

    fix(client): prevent null return in phone accessor by ensuring string type

--- src/app/Models/Client.php
+++ src/app/Models/Client.php
@@ -40,14 +40,19 @@ class Client extends Model
 
     public function getPhoneFormattedAttribute(): string
     {
-        $digits = preg_replace('/\D/', '', $this->phone);
+        if (empty($this->phone)) {
+            return '';
+        }
+
+        $digits = preg_replace('/\D/', '', $this->phone ?? '');
 
         if (strlen($digits) === 11) {
             return preg_replace('/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/', '+$1 ($2) $3-$4-$5', $digits);
         }
 
-        return $this->phone;
+        return (string) $this->phone;
     }
+
 
     public function getNumberFormattedAttribute(): string
     {
